9fa1a86bd1c835015fccab93a5adf92459b43d66
AlejandroBrnab <alejandro.rodriguez41104@gmail.com>
2026-09-21 18:22:30 -0400

    put code of previous challenge as placeholder

--- topics/prototypes/instructions/instructions-prototype-2/js/script.js
+++ topics/prototypes/instructions/instructions-prototype-2/js/script.js
@@ -1,24 +1,140 @@
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Minion
+ * Names:
+ * Agustin, Chloe, Alejandro
+ *
+ * It is a minion.
  */
 
-"use strict";
+/**
+ * Creates the canvas
+ */
+function setup() {
+  createCanvas(400, 400);
+}
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
+ * Draws the minion
+ */
+function draw() {
+  // Pink background
+  background("lightblue");
+  
+  // No stroke everywhere!
+  noStroke();
+  
+  drawMinion();
+}
+
+/**
+ * Draws the minion using functions
+ */
+function drawMinion() {
+    drawLandscape();
+    drawHair();
+    drawHead();
+    drawEyes();
+    drawMouth();
+    drawClothes();
+}
+
+/**
+ * Draws the minion's head (including its eyes, goggles and hair)
+ */
+function drawHead() {
+  
+  // Minion head
+
+  push();
+  fill("#FCE029");
+  circle(200, 200, 250);
+  
+  // Minion body
+
+  fill("#FCE029");
+  rect(75, 200, 250, 200);
+  pop();
+}
+
+/**
+ * Draws the minion's eye
+ */
+function drawEyes() {
+     // Goggle's band
+  push();
+  fill("black");
+  rect(70, 170, 260, 40);
+  pop();
+    // Goggles
+  push();
+  fill("#97978f");
+  ellipse(200, 200, 160);
+  pop();
+    // Eye
+  push();
+  fill(255);
+  ellipse(200, 200, 120);
+  fill(0);
+  ellipse(200, 200, 50);
+  pop();
 
 }
 
+/**
+ * Draws the Minion's mouth
+ */
+function drawMouth() {
+// Draw the mouth
+  push();
+  strokeWeight(7);
+  stroke(0, 20, 0);
+  noFill();
+  angleMode(DEGREES);
+  arc(200, 150, 200, 340, 65, 115);
+  pop();
+}
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
-function draw() {
+ * Draws the Minion's hair
+ */
+function drawHair() {
+    push();
+    stroke('black');
+    strokeWeight(5);
+    line(200, 150, 150, 55);
+    line(200, 150, 200, 40);
+    line(200, 150, 250, 55);
+    pop();
+}
 
+/**
+ * Draws the minion's clothes
+ */
+function drawClothes() {
+  // Minion's clothes
+  push();
+  fill("#0A75BC");
+  rect(75, 350, 250, 50);
+  pop();
+}
+
+/**
+ * Draw the mountains and sun
+ */
+
+function drawLandscape() {
+    //mountains
+    push();
+    fill("brown");
+    rect(55, 300, 50, 100);
+    pop();
+    push();
+    fill("green");
+    triangle(0, 315, 100, 20, 200, 315);
+    pop();
+
+    //sun
+    push();
+    fill("yellow");
+    circle(400, 15, 250);
 }
