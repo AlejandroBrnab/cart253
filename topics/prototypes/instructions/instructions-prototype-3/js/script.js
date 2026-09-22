3a27dc6d08dca3e645ad89fc4f83642bdc108249
AlejandroBrnab <alejandro.rodriguez41104@gmail.com>
2026-09-22 15:01:41 -0400

    started prototype 3 (3d objects)

--- topics/prototypes/instructions/instructions-prototype-3/js/script.js
+++ topics/prototypes/instructions/instructions-prototype-3/js/script.js
@@ -1,140 +1,75 @@
 /**
- * Minion
- * Names:
- * Agustin, Chloe, Alejandro
+ * An experimental prototype
+ * Name:
+ * Alejandro
  *
- * It is a minion.
+ * Really just messing around with the 3D figures.
  */
 
 /**
  * Creates the canvas
  */
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 400, WEBGL); //WEBGL so 3D shapes can be used
 }
 
-/**
- * Draws the minion
- */
 function draw() {
-  // Pink background
-  background("lightblue");
-  
-  // No stroke everywhere!
-  noStroke();
-  
-  drawMinion();
+  background(30);
+
+  drawRingFigure();
+  drawCubeFigure();
+  drawSphereFigure();
+  drawConeFigure();
+  drawCylinderFigure();
+
 }
 
-/**
- * Draws the minion using functions
- */
-function drawMinion() {
-    drawLandscape();
-    drawHair();
-    drawHead();
-    drawEyes();
-    drawMouth();
-    drawClothes();
-}
-
-/**
- * Draws the minion's head (including its eyes, goggles and hair)
- */
-function drawHead() {
-  
-  // Minion head
-
+//Draws the ring figure
+function drawRingFigure(){
   push();
-  fill("#FCE029");
-  circle(200, 200, 250);
-  
-  // Minion body
-
-  fill("#FCE029");
-  rect(75, 200, 250, 200);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  normalMaterial();
+  torus(70, 20); //different parameters of the object
   pop();
 }
 
-/**
- * Draws the minion's eye
- */
-function drawEyes() {
-     // Goggle's band
+//Draws the cube figure
+function drawCubeFigure(){
   push();
-  fill("black");
-  rect(70, 170, 260, 40);
-  pop();
-    // Goggles
-  push();
-  fill("#97978f");
-  ellipse(200, 200, 160);
-  pop();
-    // Eye
-  push();
-  fill(255);
-  ellipse(200, 200, 120);
-  fill(0);
-  ellipse(200, 200, 50);
-  pop();
-
-}
-
-/**
- * Draws the Minion's mouth
- */
-function drawMouth() {
-// Draw the mouth
-  push();
-  strokeWeight(7);
-  stroke(0, 20, 0);
-  noFill();
-  angleMode(DEGREES);
-  arc(200, 150, 200, 340, 65, 115);
+  translate(-120, -80, 0); //moving the object
+  rotateX(frameCount * 0.02);
+  rotateY(frameCount * 0.01);
+  normalMaterial();
+  box(50); //50 in all three parameters
   pop();
 }
 
-/**
- * Draws the Minion's hair
- */
-function drawHair() {
-    push();
-    stroke('black');
-    strokeWeight(5);
-    line(200, 150, 150, 55);
-    line(200, 150, 200, 40);
-    line(200, 150, 250, 55);
-    pop();
-}
-
-/**
- * Draws the minion's clothes
- */
-function drawClothes() {
-  // Minion's clothes
+//Draws the sphere figure
+function drawSphereFigure(){
   push();
-  fill("#0A75BC");
-  rect(75, 350, 250, 50);
+  translate(120, -60, 30);
+  normalMaterial();
+  sphere(35); //for all parameters
   pop();
 }
 
-/**
- * Draw the mountains and sun
- */
+//Draws the cone figure
+function drawConeFigure(){
+  push();
+  translate(-100, 100, 40);
+  rotateZ(frameCount * 0.01);
+  normalMaterial();
+  cone(30, 70); //different parameters of the object
+  pop();
+}
 
-function drawLandscape() {
-    //mountains
-    push();
-    fill("brown");
-    rect(55, 300, 50, 100);
-    pop();
-    push();
-    fill("green");
-    triangle(0, 315, 100, 20, 200, 315);
-    pop();
-
-    //sun
-    push();
-    fill("yellow");
-    circle(400, 15, 250);
+//Draws the cylinder figure
+function drawCylinderFigure(){
+  push();
+  translate(100, 100, -30);
+  rotateX(HALF_PI);
+  normalMaterial();
+  cylinder(25, 80); //different parameters of the object
+  pop();
 }
